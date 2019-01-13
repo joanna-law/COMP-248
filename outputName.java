822364c944c03b90e5de3da89c31a8b73b3be8af
Joanna Lau Ah Wing <j_lauahw@encs.concordia.ca>
2019-01-12 22:54:56 -0500

    Program that outputs my name in big using *

---
+++ outputName.java
@@ -0,0 +1,27 @@
+//---------------------------------------------------------------------
+//Assignment 1 - Program that prints my name in big letters made out of capital letters.
+//Date: 25 September 2018
+//Written by: Joanna Chin Fong Lau Ah Wing (Student ID:40083829)
+//For COMP 248 Section M-X (Nancy Acemian) - Fall 2018
+//---------------------------------------------------------------------
+
+public class outputName
+{
+	
+	//algorithm output the name that was already set in big letters.
+
+	public static void main (String[] args) {
+		System.out.println("My name is: ");
+		System.out.println();
+		System.out.println("***********************************************************************");
+		System.out.println();
+		System.out.println("     JJJJJJJJJ    OOOO        AA       N     N   N     N       AA");
+		System.out.println("         J       O    O      A  A      N N   N   N N   N      A  A");
+		System.out.println("         J       O    O     A    A     N  N  N   N  N  N     A    A");
+		System.out.println("         J       O    O    AAAAAAAA    N   N N   N   N N    AAAAAAAA");
+		System.out.println("     JJJJ         OOOO    A        A   N     N   N     N   A        A");
+		System.out.println();
+		System.out.print("***********************************************************************");
+	} //end main
+	
+} //end class
